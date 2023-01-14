aae438ff8305605d755a64341d8c87ff9f712872
Mostafa Hosseini <kavrindev@gmail.com>
2023-01-14 22:43:38 +0330

    Add koin for dependency injection * Add di package and different modules files * Add WalletlineApp.kt and setup koin

---
+++ app/src/main/java/com/walletline/di/RepositoryModule.kt
@@ -0,0 +1,13 @@
+package com.walletline.di
+
+import org.koin.dsl.module
+
+/**
+ * Repository module
+ *
+ * Data related dependencies e.g. data sources, repositories, use cases
+ */
+val repositoryModule = module {
+//    single { repository() }
+//    factory { useCases() }
+}
