d361e1fe950812e796a734b75b3ffedc78c0e3ac
atayee.prg <atayee.prg@gmail.com>
2023-02-14 12:58:40 +0330

    change iOS framework distribution to CocoaPods

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,21 +1,26 @@
 plugins {
     kotlin("multiplatform")
+    kotlin("native.cocoapods")
     id("com.android.library")
 }
 
 kotlin {
     android()
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        ios.deploymentTarget = "14.1"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
             baseName = "shared"
         }
     }
-
+    
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -50,7 +55,7 @@ android {
     namespace = "com.walletline"
     compileSdk = 33
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
         targetSdk = 33
     }
 }
